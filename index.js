041fc9274eee7feceeb38c3439a4df85ae195fb8
Valeriy Komlev <komlev88@gmail.com>
2022-08-28 12:51:31 +0200

    fix error

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ export const useDetectHorizontalOverflow = (
     let func = (entries) => {
       if (!entries.length) return null;
 
-      const r = node.getBoundingClientRect();
+      const r = container.getBoundingClientRect();
       const children = parent.childNodes;
       const result = new Set();
       children.forEach((element) => {
